8f9f073c7abee08a794c88ad1c815f9f4c8471fc
NeilMisra99 <116110498+NeilMisra99@users.noreply.github.com>
2025-02-25 19:28:50 -0500

    test line height change

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ async def download_ai_slides_pdf(request: Request):
                 }}
                 /* Reset line-height to browser defaults */
                 p, div, span, li {{
-                    line-height: normal !important;
+                    line-height: 1.5 !important;
                 }}
             ''', font_config=font_config)
             
